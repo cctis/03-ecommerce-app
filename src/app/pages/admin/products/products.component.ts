ac206a6d12b2f245cef61b4220100a30021781cb
Juan Carlos Perez Zapata <juan.perez@konexinnovation.com.co>
2024-09-06 16:24:36 -0500

    servicio de categorias proyecto ecommerce Angular

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
-  openProductModal(){}
+  isSidePanelVisible : boolean = false;
+  productObj:any ={
+    
+      "productId": 0,
+      "productSku": "string",
+      "productName": "string",
+      "productPrice": 0,
+      "productShortName": "string",
+      "productDescription": "string",
+      "createdDate": new Date(),
+      "deliveryTimeSpan": "string",
+      "categoryId": 2147483647,
+      "productImageUrl": "string",
+  }
+categoryList: any[]= [];
+
+  constructor(private productoServ: ProductService){}
+
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+
+  getAllCategory(){
+    this.productoServ.getAllCategory().subscribe((res:any)=>{
+      this.categoryList= res.data;
+      console.log()
+    });
+  }
+
+  openSidePanel(){
+    this.isSidePanelVisible=true;
+  }
+
+  closeSidePanel(){
+    this.isSidePanelVisible=false;
+  }
   onDelete(){}
   onPageChange(){}
 
---
+++ src/app/pages/services/constants/constant.ts
@@ -0,0 +1,7 @@
+export const Constant = {
+    API_END_POINT:'https://freeapi.miniprojectideas.com/api/BigBasket/',
+    METHODS:{
+        GET_ALL_PRODUCTO:'',
+        GET_ALL_CATEGORY:'GetAllCategory'
+    }
+}
